f2095b7f5329b733ec4062f4887e6281e71ab3d4
Mbhoro97 <siyaqcele11@gmail.com>
2023-03-22 02:48:14 +0200

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *main - prints _putchar and a new line
+ *Return: Success 0
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
+
+
+
+
