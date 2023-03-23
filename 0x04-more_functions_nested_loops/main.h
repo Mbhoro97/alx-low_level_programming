02eeadb53570fbf9c9484b493ac0191994d60ffb
Mbhoro97 <siyaqcele11@gmail.com>
2023-03-23 14:52:57 -0700

    This file stored prototype functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int_isupper(int c);
-int_isdigit(int c);
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
