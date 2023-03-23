1b5fae3a2d3c18e52af407316096f594e5585b1d
Mbhoro97 <siyaqcele11@gmail.com>
2023-03-23 16:10:25 -0700

    Functionn that multiplies two integers.

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_isdigit - Checks if the parameter is a number between 0 to 9.
- * @c: input number.
- * Return: returns 1 if the number between 0 to 9 or else 0.
+ *@c: input number.
+ *Return: returns 1 if the number between 0 to 9 or else 0.
  */
 int _isdigit(int c)
 {
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
