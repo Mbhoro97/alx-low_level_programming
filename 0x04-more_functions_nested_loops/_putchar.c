8d52a9c93843588012b497c90870ba3c0e283f46
Mbhoro97 <siyaqcele11@gmail.com>
2023-03-24 14:38:33 -0700

    _Putchar file for writing character in standardout

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,12 +1,9 @@
-#include "main.h"
 #include <unistd.h>
-#include <stdio.h>
-
 /**
  *_putchar - write the character c to stdout
  * @c: The character to print
  * Return 1 if is true else 0
- *  On error return -1
+ * On error return -1
  */
 int _putchar(char c)
 {
