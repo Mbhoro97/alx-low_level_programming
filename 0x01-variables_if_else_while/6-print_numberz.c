41c3c85cc143c9c21a87f005ff7ca8300abb3ac7
Mbhoro97 <siyaqcele11@gmail.com>
2023-03-25 14:40:09 -0700

    Prints digit of of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints digit numbers of base 10 starting from 0
+ * Return: Always 0 (Succesful)
+ */
+int main(void)
+{
+	int i;
+
+	for (int = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
